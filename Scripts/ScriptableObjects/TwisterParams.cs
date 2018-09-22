428a72f3db7b62003a98597f4647d6a620238ded
pf35301 <pf35301@gmail.com>
2018-09-23 06:50:16 +0900

    [add] parameter setting

--- Scripts/ScriptableObjects/TwisterParams.cs
+++ Scripts/ScriptableObjects/TwisterParams.cs
@@ -34,7 +34,11 @@ namespace TwisterForUnity {
         private uint id;
 
         public float MovePositionGain;
-        public float MoveRotateGain;
+        public float MoveRotationGain;
+
+        [Header("MIDI Fighter Twister")]
+        public byte CCMidiRangeMin;
+        public byte CCMidiRangeMax;
 
         public TwisterParams(string TwisterPortName) {
             this.TwisterPortName = TwisterPortName;
@@ -45,10 +49,16 @@ namespace TwisterForUnity {
             id = MidiJackEx.GetId(channel);
         }
     }
-    public enum RollDirection : byte {
+
+    public enum Direction3FTo41 : byte {
         Right = 0x41,
         Left = 0x3f,
         PressDown = 0x7f,
         PressUp = 0x00
+    }
+
+    public enum MidiStatus : byte {
+        Press = 0xB1,
+        Roll = 0xB0
     }
 }
